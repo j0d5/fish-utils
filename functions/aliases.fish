1ece2d158e011c8a43b5133100941bd6c36b5c78
tweetjay <tweetjay2@icloud.com>
2020-08-25 14:59:11 +0200

    Remove unnecessary echo
    Fix formatting

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -1,10 +1,14 @@
 function aliases -d 'alias definitions'
-  alias p=prevd
-  alias n=nextd
-  alias e="$EDITOR"
-  alias ...="cd ../../"
-  alias ....="cd ../../../"
-  # exa:
-  alias la="exa -abghl --git --color=automatic"
-  alias lan="la -snew"
+    alias p=prevd
+    alias n=nextd
+    alias e="$EDITOR"
+    alias ...="cd ../../"
+    alias ....="cd ../../../"
+    # exa:
+    if test (uname) = "Darwin"
+        alias la="exa -abghl --git --color=automatic"
+    else
+        alias la="ls -la"
+    end
+    alias lan="la -snew"
 end
